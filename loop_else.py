127bd4c598330752507c38df319ba0586f27a166
Paul Pham <ppham@local-box.org>
2015-03-25 19:56:36 -0700

    Add Marco Polo txt.

---
+++ loop_else.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+
+for i in range(100):
+    if (i == 50):
+        print("I reached 50")
+        break
+else:
+    print("I shouldn't even be here.")
+
---
+++ tuple_print.py
@@ -0,0 +1,7 @@
+
+def print3(a):
+    print("the first 3 numbers are: %i, %i, %i" % 
+
+print3([1,2,3])
+print3([4,5,6,7,8,9])
+
