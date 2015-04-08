b59372af6d778029eb165952e4a4463cc75c85fb
Michael Hazani <123mickey@gmail.com>
2015-04-07 18:16:48 -0700

    finish task 1

--- students/MichaelHazani/session06/html_render.py
+++ students/MichaelHazani/session06/html_render.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
-
-"""
-Python class example.
-
-"""
-
-# The start of it all:
-# Fill it all in here.
 class Element(object):
-    pass
+    """ A class that renders to HTML per the class exercise
+    """
+    tag = 'html'
+    indent = '    '
+
+    def __init__(self, content=None):
+        if content:
+            self.content += content
+        else:
+            self.content = ""
+
+    def append(self, element_to_append):
+        self.content += (self.indent + element_to_append)
+
+    def render(self, file_out, ind=""):
+        outstring = (self.indent + "<" + self.tag + ">\n"
+                     + self.indent + self.content + "\n"
+                     + self.indent + "</" + self.tag + ">")
+        file_out.write(outstring)
