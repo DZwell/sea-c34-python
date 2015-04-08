0a92890438d16e1dab9c0e03982eefea0a696ade
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-04-08 13:56:05 -0700

    complete step 1

--- students/JeffreyMendiola/session06/html_render.py
+++ students/JeffreyMendiola/session06/html_render.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 
-"""
-Python class example.
 
-"""
-
-# The start of it all:
-# Fill it all in here.
 class Element(object):
-    pass
+    """
+    Step 1: Create an Element class for rendering an html element (xml element)
+    """
+    tag_name = "html"
+    indent = "    "
+
+    def __init__(self, content=None):
+        self.content = self.indent + str(content) if content else ""
+
+    def append(self, string):
+        self.content += (
+            "{indent}{string}\n".format(indent=self.indent, string=string)
+        )
+
+    def render(self, file_out, line=""):
+        file_out.write(
+            "{indent}<{tag}>\n"
+            "{indent}{content}"
+            "{indent}</{tag}>"
+            .format(indent=line, tag=self.tag_name, content=self.content)
+        )
