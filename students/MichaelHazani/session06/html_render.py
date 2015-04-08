505470d7602c439220f7cdccdb068cdeb0142461
Michael Hazani <123mickey@gmail.com>
2015-04-07 20:19:43 -0700

    task17 all the way to part 5

--- students/MichaelHazani/session06/html_render.py
+++ students/MichaelHazani/session06/html_render.py
@@ -2,11 +2,15 @@
 class Element(object):
     """ A class that renders to HTML per the class exercise
     """
-    openingtag = '<>'
-    closingtag = '</>'
+    openingtag = '<'
+    closingtag = '</'
     indent = '    '
+    c = ">"
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
+
+        self.atts = kwargs
+
         if content:
             self.children = [content]
         else:
@@ -17,34 +21,77 @@ class Element(object):
 
     def render(self, file_out, ind=""):
         file_out.write("\n" + ind + self.openingtag)
+        for atri, val in self.atts.items():
+            file_out.write(" %s=\"%s\"" % (atri, val))
+        file_out.write(self.c)
         for child in self.children:
             try:
                 child.render(file_out, self.indent + ind)
             except AttributeError:
                 file_out.write("\n")
                 file_out.write(ind + self.indent + child)
-        file_out.write("\n" + ind + self.closingtag)
+        file_out.write("\n" + ind + self.closingtag + self.c)
 
 
 class P(Element):
 
-    openingtag = "<p>"
-    closingtag = "</p>"
+    openingtag = "<p"
+    closingtag = "</p"
 
 
 class Html(Element):
 
-    openingtag = "<html>"
-    closingtag = "</html>"
+    openingtag = "<html"
+    closingtag = "</html"
 
 
 class Body(Element):
 
-    openingtag = "<body>"
-    closingtag = "</body>"
+    openingtag = "<body"
+    closingtag = "</body"
+
+
+class OneLineTag(Element):
+
+    def render(self, file_out, ind=""):
+            file_out.write("\n" + ind + self.openingtag + self.c)
+            for child in self.children:
+                try:
+                    child.render(file_out)
+                except AttributeError:
+                    file_out.write(child)
+            file_out.write(self.closingtag + self.c)
+
+
+class Title(OneLineTag):
+
+    openingtag = "<title"
+    closingtag = "</title"
 
 
 class Head(Element):
 
-    openingtag = "<head>"
-    closingtag = "</head>"
+    openingtag = "<head"
+    closingtag = "</head"
+
+
+class SelfClosingTag(Element):
+
+    def render(self, file_out, ind=""):
+        file_out.write("\n" + ind + self.openingtag)
+        for atri, val in self.atts.items():
+            file_out.write(" %s=\"%s\"" % (atri, val))
+        for child in self.children:
+            try:
+                child.render(file_out, self.indent + ind)
+            except AttributeError:
+                file_out.write("\n")
+                file_out.write(ind + self.indent + child)
+
+
+class Hr(SelfClosingTag):
+    openingtag = "<hr />"
+
+
+class Br(SelfClosingTag):
+    openingtag = "<br />"
