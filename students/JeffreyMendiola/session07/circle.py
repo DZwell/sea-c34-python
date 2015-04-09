7678628ca332a08e31526fff0efe3d3a4c4bdbea
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-04-08 19:34:42 -0700

    class work

--- students/JeffreyMendiola/session07/circle.py
+++ students/JeffreyMendiola/session07/circle.py
@@ -7,5 +7,16 @@ import math
 
 
 class Circle(object):
+
     def __init__(self, radius):
         self.radius = radius
+        self._diameter = radius * 2
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, value):
+        self.radius = value / 2
+        self._diameter = value
