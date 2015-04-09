b84c3da9678fb58b822d213c299fd14fcd1e0bee
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-04-08 18:25:26 -0700

    complete task 2

--- students/JeffreyMendiola/session06/html_render.py
+++ students/JeffreyMendiola/session06/html_render.py
@@ -9,17 +9,38 @@ class Element(object):
     indent = "    "
 
     def __init__(self, content=None):
-        self.content = self.indent + str(content) if content else ""
+        if content:
+            self.children = [content]
+        else:
+            self.children = []
 
     def append(self, string):
-        self.content += (
-            "{indent}{string}\n".format(indent=self.indent, string=string)
-        )
+        self.children.append(string)
 
     def render(self, file_out, line=""):
-        file_out.write(
-            "{indent}<{tag}>\n"
-            "{indent}{content}"
-            "{indent}</{tag}>"
-            .format(indent=line, tag=self.tag_name, content=self.content)
-        )
+        file_out.write(line + "<" + self.tag_name + ">\n")
+
+        for child in self.children:
+            try:
+                child.render(file_out, self.indent + line)
+            except AttributeError:
+                file_out.write(self.indent + line + child + "\n")
+
+        file_out.write(line + "</" + self.tag_name + ">\n")
+
+
+class Html(Element):
+    tag_name = "html"
+    doctype_decl = "<!DOCTYPE>\n"
+
+    def render(self, file_out, line=""):
+        file_out.write(self.doctype_decl)
+        Element.render(self, file_out, line="")
+
+
+class Body(Element):
+    tag_name = "body"
+
+
+class P(Element):
+    tag_name = "p"
