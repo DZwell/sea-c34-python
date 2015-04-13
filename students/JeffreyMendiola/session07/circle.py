d16e7045f2750ea0f5fa366afe4bbc8e6ff0aafe
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-04-13 13:29:52 -0700

    finish diameter & area tests

--- students/JeffreyMendiola/session07/circle.py
+++ students/JeffreyMendiola/session07/circle.py
@@ -11,6 +11,7 @@ class Circle(object):
     def __init__(self, radius):
         self.radius = radius
         self._diameter = radius * 2
+        self._area = math.pi * (radius ** 2)
 
     @property
     def diameter(self):
@@ -20,3 +21,7 @@ class Circle(object):
     def diameter(self, value):
         self.radius = value / 2
         self._diameter = value
+
+    @property
+    def area(self):
+        return self._area
