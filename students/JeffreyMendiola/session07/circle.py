3cf8e5045b7d6a0d7fd5517390099e5ca969ff78
Jeffrey Mendiola <jwmendio@yahoo.com>
2015-04-13 14:27:56 -0700

    complete circle tests

--- students/JeffreyMendiola/session07/circle.py
+++ students/JeffreyMendiola/session07/circle.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
-"""circle class --
-
-fill this in so it will pass all the tests.
 """
+===============================================================================
+file: circle.py
+===============================================================================
+Programmer: Jeffrey Mendiola
+Date: 04/01/2015
+Course: SEA-C34: Foundations II: Python
+Time: MW 7:00pm - 9:00pm
+Instructor: Paul Pham
+Task: #19 Testing, Testing, 1 2 3
+Description:
+    Create a circle class that passes all the tests.
+===============================================================================
+"""
+
 import math
 
 
@@ -25,3 +36,23 @@ class Circle(object):
     @property
     def area(self):
         return self._area
+
+    def __str__(self):
+        return("Circle with radius: %.6f" % self.radius)
+
+    def __repr__(self):
+        return("Circle(%i)" % self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, other):
+        return Circle(self.radius * other)
+
+    def __cmp__(self, other):
+        if self._area > other._area:
+            return 1
+        elif self._area < other._area:
+            return -1
+        else:
+            return 0
